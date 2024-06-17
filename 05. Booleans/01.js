3163ee399e566c80baa99f0cec463f2cf8695de1
Jose <64285855+jreycid@users.noreply.github.com>
2024-06-17 14:14:30 +0200

    Add files via upload

---
+++ 05. Booleans/01.js
@@ -0,0 +1,20 @@
+/*
+ * Os booleano soamente admiten dous valores: verdadeiro e falso
+ * Son útiles para comprobar o estado dunha condición ou variable
+ *
+ */
+
+console.log(Boolean(1));
+console.log(Boolean("Whatever"));
+console.log(Boolean(3.14));
+console.log(Boolean(100 > 5));
+console.log(Boolean(1 < 100));
+console.log(Boolean("1" == 1));
+
+console.log(Boolean(0));
+console.log(Boolean(-0));
+console.log(Boolean(""));
+console.log(Boolean(NaN));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean("1" === 1));
