53f33e4dd4232cec675ef8a78ce333ceec706678
Jose <64285855+jreycid@users.noreply.github.com>
2024-06-20 21:37:45 +0200

    Add files via upload

---
+++ 05. Tipos_primitivos/00. Intro/01.js
@@ -0,0 +1,58 @@
+/*
+    O estándar ECMAScript máis recente define nove tipos:
+
+        Seis tipos de datos primitivos:
+
+            1. Número: typeof instance === "number"
+            2. Cadena: typeof instance === "string"
+            3. BigInt: typeof instance === "bigint"
+            4. Símbolo: typeof instance === "symbol"
+            5. Booleano: typeof instance === "boolean"
+            6. Indefinido: typeof instance === "undefined"
+
+        Adicionalmente existe un tipo adicional,
+        que se podería considerar primitivo:
+
+            7. Nulo: typeof instance === "object".
+                
+        Un tipo primitivo é aquel que non é un obxecto, 
+        polo que non ten métodos nin propiedades.
+        As variables de calquera tipo primitivo son inmutables,
+        é dicir, non se poden modificar. Cando a unha variable ou constante
+        de tipo primitivo que xa ten asignado un valor, se lle intenta 
+        asignar outro valor, o que fai javascript é crear unha nova instancia da variable.
+            
+        En Javascript, toda variable ou constante quee non pertence a un tipo primitivo,
+        é un obxecto, incluso as funcións son obxectos e como tal poden tratarse.
+
+            8. Obxecto: typeof instance === "object". (Exemplo: Array, Map, Date, etc.)
+
+            9. Función: unha estrutura sen datos, typeof instance === "function".
+                        As funcións en Javascript son obxectos de tipo Function.
+
+    O único propósito valioso de usar o operador typeof é comprobar o tipo de datos.
+    Se queremos comprobar calquera tipo estrutural derivado de Object,
+    non ten sentido usar typeof para iso, xa que sempre recibiremos "obxecto".
+    A forma correcta de comprobar que tipo de obxecto estamos a usar é a palabra clave instanceof.
+
+*/
+
+/*
+    Autoboxing ou Object wrapper
+
+    O feito de que poidamos chamar funcións desde tipos de datos primitivos
+    non é porque estes datos teñan acceso a esas funcións, senón por "Autoboxing".
+
+    O Autoboxing ou Object Wrapper prodúcese cando chamamos a un método
+    definido no prototipo relacionado co primitivo en cuestión.
+
+*/
+    let saudo = 'Ola frontenders!';
+
+    console.log(saudo);
+    console.log(saudo.toUpperCase);
+
+// No exemplo anterior, o método toUpperCase defínese no prototipo String,
+// que está relacionado co valor primitivo 'saúdo'.
+// No momento en que chamas ao método prototipo, o motor JavaScript encapsula o valor do primitivo,
+// executa a función e destrúe este obxecto usado.
---
+++ 08. Estruturas_de_control/01.js
@@ -0,0 +1,142 @@
+/*
+    EXEMPLOS ESTRUTURAS DE CONTROL DE FLUXO
+*/
+
+//if simple
+let numero = 4;
+if (numero > 3) {
+    console.log("\né mayor que 3");
+}
+
+//if -else
+if (numero >= 3) {
+    console.log("\né maior ou igual que 3");
+} else {
+    console.log("\né menor que 3");
+}
+
+//if else if
+if (numero == 3) {
+    console.log("\né igual que 3");
+} else if (numero > 3) {
+    console.log("\né maior que 3");
+}
+
+// Operador ternario: (condición ? verdadero : falso)
+let nota = 7;
+
+let calificacion = nota < 5 ? "suspenso" : "aprobado";
+console.log("\nO resultado do exame é:", calificacion);
+
+let notaFinal =
+    nota < 5
+        ? "Insuficiente"
+        : nota < 6
+        ? "Suficiente"
+        : nota < 8
+        ? "Ben"
+        : nota <= 9
+        ? "Notable"
+        : "Sobresaiente";
+
+console.log("A nota final é:", notaFinal);
+
+//sentencia switch
+let idade = 20;
+switch (idade) {
+    case 10:
+        console.log("\nten 10 anos");
+        break;
+    case 18:
+        console.log("\né maior de idade");
+        break;
+    case 30:
+        console.log("\né un adulto");
+        break;
+    default:
+        console.log("\nten " + idade + " anos");
+        break;
+}
+
+//bucle while
+console.log("\nbucle while: ");
+let i = 0;
+while (i < 10) {
+    console.log(i);
+    i++;
+}
+
+//bucle for
+console.log("\nbucle for: ");
+for (i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+//bucle do-while
+i = 0;
+console.log("\nbucle do-while: ");
+do {
+    console.log(i);
+    i++;
+} while (i < 10);
+
+//bucle for in
+console.log("\nbucle for in: ");
+let modulos = ["FOL", "SINF", "CD", "PROG", "BD", "LIMA"];
+for (let modulo in modulos) {
+    console.log(modulo);
+    //console.log('modulos[' ,modulo ,'] = ', modulos[modulo]);
+}
+
+//bucle for of
+console.log("\nbucle for of: ");
+for (modulo of modulos) {
+    console.log(modulo);
+}
+
+/*
+    for-in vs for-of
+
+    Simplificando podemos dicir que:
+
+        for in -> itera sobre chaves
+        for of -> itera sobre valores
+*/
+
+modulos = [
+    {
+        nome: "PROG",
+        horas: 240,
+        profesor: "Amador",
+    },
+    {
+        nome: "BD",
+        horas: 180,
+        profesor: "Jose",
+    },
+    {
+        nome: "LIMA",
+        horas: 120,
+        profesor: "Luis",
+    },
+];
+
+console.log("\nbucle for in: ");
+for (modulo in modulos) {
+    console.log(modulo);
+}
+
+console.log("\nbucle for of: ");
+for (modulo of modulos) {
+    console.log(modulo);
+}
+
+//try-catch-finnaly
+console.log("\nbloque try-catch: ");
+try {
+    funcion_que_non_existe();
+} catch (ex) {
+    console.log("Erro detectado: " + ex.description);
+} finally {
+    console.log("faino de todas formas");
+}
