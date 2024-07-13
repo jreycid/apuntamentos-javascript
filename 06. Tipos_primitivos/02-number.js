a0757beb557c079519864121222423583e8e8d7b
Jose <64285855+jreycid@users.noreply.github.com>
2024-07-14 00:58:07 +0200

    Add files via upload

---
+++ 06. Tipos_primitivos/02-number.js
@@ -0,0 +1,64 @@
+/*
+    A diferenza doutras linguaxes de programación,
+    en JavaScript só hai un xeito de representar calquera tipo de número,
+    ben sexa enteiro ou con decimais (tocaremos bigInt máis adiante)
+*/
+console.log(typeof 42); // 'number'
+console.log(typeof 12.2); // 'number'
+console.log(typeof -24); // 'number'
+
+// + Sumar
+console.log(2 + 2);
+// - Restar
+console.log(3 - 1);
+// * Multiplicar
+console.log(3 * 2);
+// / Dividir
+console.log(4 / 2);
+// < Menor que
+console.log(10 < 20);
+// > Maior que
+console.log(40 > 20);
+// <= Menor ou igual que
+console.log(10 <= 20);
+// >= Maior ou igual que
+console.log(50 >= 40);
+// == Igual que (Comproba os valores)
+console.log(40 == 40); // TRUE
+console.log(40 == "40"); // TRUE
+// === Igual que (Comproba os valores e os tipos)
+console.log(40 === 40); // TRUE
+console.log(40 === "40"); // FALSE
+// != Non é igual que
+// !== O mesmo que ===, comproba valor e tipo
+console.log(30 !== 30);
+
+/*
+ * OPERACIONS:
+ * typeof
+ * NaN
+ * isNaN()
+ * toString()
+ * toString()
+ * .toFixed()
+ */
+
+// typeof
+let num = 20;
+console.log(typeof num);
+// NaN
+const notNumber = 2 + "asfda";
+console.log(typeof notNumber);
+// isNaN()
+isNaN(NaN);
+console.log(isNaN(NaN));
+console.log(isNaN("whatever"));
+console.log(isNaN("12"));
+console.log(isNaN(12));
+// toString()   para pasar de número a string
+let number = 100.1164123412;
+console.log(number.toString());
+console.log(typeof number.toString());
+// .toFixed()   para redondear ou recortar decimales dun número
+console.log(number.toFixed);
+console.log(number.toFixed(2));
---
+++ 06. Tipos_primitivos/04-bigint.js
@@ -0,0 +1,20 @@
+/*
+    Permítenos utilizar calquera número enteiro sen límite de tamaño.
+    Isto significa que xa non temos a limitación que atopamos co tipo number.
+*/
+
+// Para declarar un valor de tipo bigint,
+// necesitamos engadir un n ao final do número desexado:
+
+const verylargenumber = 123456789n; // a "n" ao final é importante!
+
+// En canto ás operacións matemáticas,
+// non está permitido facelas entre distintos tipos de datos,
+// só é posible se ambos os números son do tipo bigint:
+
+const n1 = 67890987654323456789n;
+// tamén podemos crealos a partir do obxecto "BigInt"
+const n2 = BigInt(42);
+
+console.log(n1 + n2); // ✅ VÁLIDO
+//console.log(n1 + 100) // 🚫 NON VÁLIDO
