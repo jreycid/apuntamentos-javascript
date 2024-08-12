65ec185a3c99b6884af72ebcb95b8de28aa6ebb0
Jose <64285855+jreycid@users.noreply.github.com>
2024-08-13 00:29:24 +0200

    Add files via upload

--- 96. AdventJS_2023/07/07.js
+++ 96. AdventJS_2023/07/07.js
@@ -10,7 +10,6 @@
 
     drawGift(4, '+')
 
-
     ####
     #++##
     #++#+#
@@ -43,3 +42,37 @@
     Nota: Ten en cuenta que, en los tests, la primera línea se ve empujada
     por el caracter ".
 */
+
+function drawGift(size, symbol) {
+    let bgSize = size - 2;
+
+    bgSize += +!(bgSize + 1);
+
+    let response = "";
+
+    let topCenter = "";
+    let bottomCenter = "";
+
+    for (const a of [...Array.from({ length: bgSize }).keys()]) {
+        const c = "#" + symbol.repeat(bgSize) + "#" + symbol.repeat(a) + "#";
+        bottomCenter = c + "\n" + bottomCenter;
+        topCenter += " ".repeat(bgSize - a) + c + "\n";
+    }
+
+    response =
+        " ".repeat(size - 1) +
+        "#".repeat(size) +
+        "\n" +
+        (
+            topCenter +
+            "#".repeat(size) +
+            symbol.repeat(bgSize) +
+            "#" +
+            "\n" +
+            bottomCenter +
+            "#".repeat(size) +
+            "\n"
+        ).repeat(+!!(size - 1));
+
+    return response;
+}
